f3e4c94578b87b12a09a0b1676bfd9318abdf331
reema-eilouti <reema.eilouti@gmail.com>
2021-07-11 12:43:20 +0300

    added views

--- edu_hub/admin.py
+++ edu_hub/admin.py
@@ -1,8 +1,8 @@
 from django.db import models
-from edu_hub.models import Connection, Member, Message, MessageRecipient, Post
+from edu_hub.models import Connection, Member, Message, Chat, Post
 from django.contrib import admin
 
-models = (Member, Message, MessageRecipient, Post, Connection)
+models = (Member, Message, Chat, Post, Connection)
 
 for model in models:
 
--- edu_hub/views.py
+++ edu_hub/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
-from edu_hub.models import Member, Message, MessageRecipient, Post, Connection
+from rest_framework import generics
+from edu_hub.models import Member, Message, Chat, Post, Connection
+
+
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
 
 
 
+class ConnectionList(generics.ListCreateAPIView):
+    queryset = Connection.objects.all()
+
+class ConnectionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Connection.objects.all()
+
+
+
+class ChatList(generics.ListCreateAPIView):
+    queryset = Chat.objects.all()
+
+class ChatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chat.objects.all()
+
+
+
+class MessageList(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
