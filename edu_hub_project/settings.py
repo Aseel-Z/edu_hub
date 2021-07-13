d24dee4366d82603bc76f3b8999035ec861281a5
reema-eilouti <reema.eilouti@gmail.com>
2021-07-13 16:21:10 +0300

    added postgres

--- edu_hub_project/settings.py
+++ edu_hub_project/settings.py
@@ -87,12 +87,23 @@ WSGI_APPLICATION = 'edu_hub_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'edu_hub_database',
+        'USER' : 'edu_hub_user',
+        'PASSWORD' : '1234eduhub',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+        }
+        }
 
 
 # Password validation
