cfd665dfda4d6f4d9c5510444cf067bbe817e530
reema-eilouti <reema.eilouti@gmail.com>
2021-07-14 12:19:32 +0300

    added working tests

--- edu_hub/tests.py
+++ edu_hub/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from edu_hub.models import Message, Connection, Post
 
-# Create your tests here.
+class PostTests(APITestCase):
+
+    def test_home_page_status_code(self):
+        url = reverse('member_list')
+        response = self.client.get(url) 
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        url = reverse('member_detail', args = [1,])
+        response = self.client.get(url)
+        # It gives 401 because the authentication. 
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_show_connections(self):
+        url = reverse("show_connections")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_show_connections_primary(self):
+        url = reverse('show_connections_primary', args = [1,])
+        response = self.client.get(url)
+        # It gives 401 because the authentication. 
+        self.assertEqual(response.status_code, 404)
+
+        
+    def test_show_message(self):
+        url = reverse('show_message')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_show_message_primary(self):
+        url = reverse('show_message_primary', args = [1,])
+        response = self.client.get(url)
+        # It gives 401 because the authentication. 
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_show_post(self):
+        url = reverse('show_post')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_show_post_primary(self):
+        url = reverse('show_post_primary', args = [1,])
+        response = self.client.get(url)
+        # It gives 401 because the authentication. 
+        self.assertEqual(response.status_code, 404)
